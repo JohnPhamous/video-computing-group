dd8acf34ed5293bf087dc7714ba57934c097217c
John Pham <johnphammail@gmail.com>
2017-09-18 19:19:03 -0700

    Adding header

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="header" class="container">
+    <div class="row inherit-height">
+      <div class="col-md-1 col-xs-2 inherit-height">
+        <img id="ucr-logo" class="img-fluid vc" src="../../static/ucr-logo.png" alt="UCR's Logo"/>
+      </div>
+      <div id="header-title" class="col-md-11 col-xs-10 inherit-height">
+        <p class="vc">
+          <span class="primary no-margin h2">Video Computing Group</span>
+          <br/>
+          Developing the Premier Computing Vision Algorithms
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AmitHeader'
+}
+</script>
