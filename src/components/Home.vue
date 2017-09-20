2301c97ca84eae50964c2632948de36475656039
John Pham <johnphammail@gmail.com>
2017-09-20 11:27:46 -0700

    Homepage layout done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,174 @@
 <template>
-  <div id="header" class="container">
-    <div class="row inherit-height">
-      <div class="col-md-1 col-xs-2 inherit-height">
-        <img id="ucr-logo" class="img-fluid vc" src="../../static/ucr-logo.png" alt="UCR's Logo"/>
+  <div class="container inherit-height">
+    <div id="header">
+      <div class="row inherit-height">
+        <div class="col-md-1 col-xs-2 inherit-height">
+          <img id="ucr-logo" class="img-fluid vc" src="../../static/ucr-logo.png" alt="UCR's Logo"/>
+        </div>
+        <div id="header-title" class="col-md-11 col-xs-10 inherit-height">
+          <p class="vc">
+            <span class="primary no-margin h2">Video Computing Group</span>
+            <br/>
+            Developing the Premier Computing Vision Algorithms
+          </p>
+        </div>
       </div>
-      <div id="header-title" class="col-md-11 col-xs-10 inherit-height">
-        <p class="vc">
-          <span class="primary no-margin h2">Video Computing Group</span>
-          <br/>
-          Developing the Premier Computing Vision Algorithms
-        </p>
+    </div>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Publications</a></li>
+            <li><a href="#">Datasets</a></li>
+            <li><a href="#">People</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Research Projects<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Overview</a></li>
+                <li><a href="#">Human Robot Vision Network</a></li>
+                <li><a href="#">Wide Area Scene Analysis in Vision Network</a></li>
+                <li><a href="#">Activity Recognition and Prediction</a></li>
+                <li><a href="#">Situational Awareness Under Resource Constraints</a></li>
+                <li><a href="#">Face Tracking and Recognition</a></li>
+                <li><a href="#">Biological Image Analysis</a></li>
+                <li><a href="#">VideoWeb: A Video Network Lab</a></li>
+                <li><a href="#">Multi-Terminal Video Compression</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <div id="home-body">
+      <div class="row header-parent">
+        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/amitrc_cover.jpg" alt="The Team"></div>
+        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/image3.png" alt=""></div>
+        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/image4.png" alt=""></div>
+        <div class="col-md-3 header-img-container"><img class="header-img" src="../../static/img/image5.png" alt=""></div>
       </div>
+      <div class="home-body">
+        <div class="row">
+          <div class="col-md-8">
+            <h3>
+              The Video Computing Group at UC Riverside conducts cutting edge research in computer vision, image processing, pattern recognition, statistical learning, and statistical processing.
+            </h3>
+            <p>
+              Current projects are related to camera networks, human activity recognition and recognition, and bioimage analysis. The work provides the scientific underpinning behind applications capable of automated/semi-automated analysis of the 3D environment from images/videos, analogous to capabilities of biological visual systems. Members of the group regularly publish in top-tier conferences and journals in computer vision and image processing. Past members work in major research labs and hold faculty positions across the world.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h3>
+              Open Positions
+            </h3>
+            <p>
+              If you are interested in a position feel free to contact Dr. Amit Chowdhury
+            </p>
+            <ul>
+              <li>Undergraduates</li>
+              <li>Graduates</li>
+              <li>Post-Doc</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div class="row">
+          <div class="col-md-3">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+            <a href="#">
+              Diversity-aware Multi-Video Summarization
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+            <a href="#">
+              Diversity-aware Multi-Video Summarization
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+            <a href="#">
+              Diversity-aware Multi-Video Summarization
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+            <a href="#">
+              Diversity-aware Multi-Video Summarization
+            </a>
+          </div>
+        </div>
+        <div id="sponsors" class="row text-center">
+          <h3>Acknowledgements</h3>
+          <p>The Video Computing Group graciously acknowledges the funding received from a number of government agencies and private corporations.</p>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2 col-md-offset-1">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+          <div class="col-md-2">
+            <a href="#"><img class="news" src="../../static/img/image3.png" alt=""></a>
+          </div>
+        </div>
+      </div>
+     <footer class="footer">
+      <div class="container">
+        <div class="row" style="margin-top:25px;">
+          <div class="col-md-4">
+            <p class="text-muted">
+              Department of Electrical and Computer Engineering<br/>
+              University of California, Riverside<br/>
+              Copyright &copy; 2017 Video Computing Group
+            </p>
+          </div>
+          <div class="col-md-4">
+            <p class="text-muted">
+              Video Computing Group<br/>
+              Winston Chung Hall 343 <br/>
+              <a href="mailto:amitrc@ece.ucr.edu">amitrc@ece.ucr.edu</a>
+            </p>
+          </div>
+          <div class="col-md-4">
+            <span class="text-muted" style="color:red;">Which logos would you like here?</span>
+          </div>
+        </div>
+      </div>
+    </footer>
     </div>
   </div>
 </template>
